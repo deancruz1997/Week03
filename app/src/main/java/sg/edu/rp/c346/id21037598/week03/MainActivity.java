895dd74092ec1576f9c2df5e9771c98f619168d4
deancruz1997 <cruzdean1997@gmail.com>
2023-05-04 12:04:08 +0800

    Complete up to section D of WS session 1

--- app/src/main/java/sg/edu/rp/c346/id21037598/week03/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21037598/week03/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tvDisplay;
     Button btnDisplay;
     EditText etInput;
+    ToggleButton tbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         tvDisplay = findViewById(R.id.textViewDisplay);
         btnDisplay = findViewById(R.id.buttonDisplay);
         etInput = findViewById(R.id.editTextInput);
+        tbtn = findViewById(R.id.toggleButtonEnabled);
 
         btnDisplay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +34,22 @@ public class MainActivity extends AppCompatActivity {
                 tvDisplay.setText(stringResponse);
             }
         });
+
+        tbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String stringResponse = tbtn.getText().toString();
+
+                if (!tbtn.isChecked()) {
+                    btnDisplay.setEnabled(false);
+                    etInput.setEnabled(false);
+                } else {
+                    btnDisplay.setEnabled(true);
+                    etInput.setEnabled(true);
+                }
+
+            }
+        });
+
     }
 }
